59a819c6c87f410fa2d7c30ab4e3b0097776843b
Anshu <anshu.aj.joshi@gmail.com>
2022-05-15 11:46:38 +0530

    sign in sign up

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const body=document.querySelector('body');
+const signbtn=document.querySelector('.sign-in-sign-up');
 window.addEventListener('load',()=>{
     body.classList.add('visible');
+})
+
+signbtn.addEventListener('click',()=>{
+    location.href='./pages/signInsignUp/authenticate.html';
 })
---
+++ pages/signInsignUp/index.js
@@ -0,0 +1,52 @@
+const body=document.querySelector('body');
+const apiUrl = "https://afternoon-headland-23698.herokuapp.com";
+
+window.addEventListener("load",()=>{
+    body.classList.add("visible");
+});
+
+const signInform=document.querySelector('.signin-form');
+
+signInform.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    const signInEmail=document.querySelector(".signin-email");
+    const signInPassword=document.querySelector('.signin-password');    
+
+    const email=signInEmail.value;
+    const password=signInPassword.value;
+
+    location.href='../dashboard/dashboard.html'
+})
+
+const signUpform=document.querySelector('.signup-form');
+signUpform.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    
+  const email = document.querySelector(".signup-email").value;
+  const name = document.querySelector(".signup-name").value;
+  const password = document.querySelector(".signup-password").value;
+  const retypedPassword = document.querySelector(
+    ".signup-retyped-password"
+  ).value;
+
+  if (password !== retypedPassword) {
+    alert("Passwords don't match");
+    return;
+  }
+
+  fetch(`${apiUrl}/auth/signup`,{
+      method:'POST',
+      headers:{
+      "Content-Type":"application/json",
+      },
+      body:JSON.stringify({name,email,password}),
+  })
+  .then((res)=>res.json())
+  .then((data)=>{
+      console.log(data);
+  })
+  .catch((err)=>{
+    //   console.log(err);
+  })
+})
