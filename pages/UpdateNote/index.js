3a7fbff061252c41b9ca92dc9f1e643967b13c13
Anshu <anshu.aj.joshi@gmail.com>
2022-05-16 12:52:16 +0530

    updated notes done

--- pages/UpdateNote/index.js
+++ pages/UpdateNote/index.js
@@ -1,11 +1,48 @@
-const inputval=document.querySelector('.create-note-input');
+// const inputval=document.querySelector('.create-note-input');
 
-inputval.addEventListener('input',(e)=>{
-    console.log(e.target.value);
+// inputval.addEventListener('input',(e)=>{
+//     console.log(e.target.value);
+// });
+
+// //to fetch the id 
+// const urlSearchParams = new URLSearchParams(window.location.search);
+// const nodeId = urlSearchParams.get("IdNumber"); //jis naam se apan ne bheja hai url main in a tag (see js)
+
+// console.log(nodeId);
+  
+const urlParams = new URLSearchParams(window.location.search);
+const noteId = urlParams.get("noteId");
+
+console.log(noteId);
+
+const updateNoteButton = document.querySelector(".create-note-button");
+
+const apiUrl = "http://localhost:8000";
+
+const token = localStorage.getItem("jwt");
+
+updateNoteButton.addEventListener("click", () => {
+  const content = document.querySelector(".create-note-input").value;
+  const heading = document.querySelector(".create-note-heading").value;
+
+  if (token) {
+    fetch(`${apiUrl}/note/update/${noteId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: token,
+      },
+      body: JSON.stringify({ content, heading }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          location.href = "/pages/dashboard/dashboard.html";
+        }
+      })
+      .catch((err) => {
+        alert("Error Creating Note!! Re-try....");
+        console.log(err);
+      });
+  }
 });
-
-//to fetch the id 
-const urlSearchParams = new URLSearchParams(window.location.search);
-const nodeId = urlSearchParams.get("IdNumber"); //jis naam se apan ne bheja hai url main in a tag (see js)
-
-console.log(nodeId);
